212aa3415f150f8c7246186320aac26262907aa0
snagrath-sys <shriyanagrathpm1997@gmail.com>
2020-06-12 21:42:35 +0530

    .

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -22,13 +22,34 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+i=1;
+C=[0.01,0.03,0.1,0.3,1,3,10,30];
+sigma=[0.01,0.03,0.1,0.3,1,3,10,30];
+index_min=1;
+
+for index=1:length(C)
+	
+	for sigma_index=1:length(sigma)
+		
+		model=svmTrain(X,y,C(index),@(x1,x2)gaussianKernel(x1,x2,sigma(sigma_index)));
+		predictions=svmPredict(model,Xval);
+		error_(i)=mean(predictions~=yval);
+		if(i!=1)
+			if(error_(i)<error_(index_min))
+				C_Sel=C(index);
+				sigma_Sel=sigma(sigma_index);
+				index_min=i;
+			end       
+		end
+		i=i+1;
+		
+	endfor	
+	
+endfor
 
 
-
-
-
-
-
+C=C_Sel;
+sigma=sigma_Sel;
 % =========================================================================
 
 end
